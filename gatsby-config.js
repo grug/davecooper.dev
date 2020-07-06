7597097c83dfeba1ab6a8f3a1d2269d89f7d2e3e
Dave Cooper <dave@davecooper.org>
2020-07-06 13:17:35 +0100

    Add goatcounter

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,6 +129,12 @@ module.exports = {
       },
     },
     'gatsby-plugin-dark-mode',
+    {
+      resolve: `gatsby-plugin-goatcounter`,
+      options: {
+        code: 'dave',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
